663199d606cd1eb29fe95780a27cda89f06868c8
Art Navsegda <artnavsegda@gmail.com>
2016-06-17 19:50:38 +0300

    pca

--- src/i2ccontrol.c
+++ src/i2ccontrol.c
@@ -5,6 +5,55 @@
 #include <linux/i2c-dev.h>
 #include <string.h>
 
+#define PCA9557_DIRECTION_REGISTER 0x03
+#define PCA9557_OUTPUT_REGISTER 0x01
+#define PCA9557_INPUT_REGISTER 0x00
+#define PCA9557_BASE_ADDRESS 0x18
+#define PCA9557_DIR_INPUT 0x01
+#define PCA9557_DIR_OUTPUT 0x00
+#define PCA9557_PIN_LEVEL_LOW 0x00
+#define PCA9557_PIN_LEVEL_HIGH 0x01
+
+void pca9557_init(uint8_t addr)
+{
+	// output all bits level low
+	i2c_send(&TWIE, PCA9557_BASE_ADDRESS+addr, 0x01, 0x00);
+	// polarity all bits retained
+	i2c_send(&TWIE, PCA9557_BASE_ADDRESS+addr, 0x02, 0x00);
+	// direction all bits input
+	i2c_send(&TWIE, PCA9557_BASE_ADDRESS+addr, PCA9557_DIRECTION_REGISTER, 0xFF);
+}
+
+void pca9557_set_pin_dir(uint8_t addr, uint8_t port, uint8_t direction)
+{
+	uint8_t state;
+	state = i2c_read(&TWIE,PCA9557_BASE_ADDRESS+addr,PCA9557_DIRECTION_REGISTER);
+	if (direction == PCA9557_DIR_INPUT)
+		state = state | port;
+	else if (direction == PCA9557_DIR_OUTPUT)
+		state = state & port;
+	i2c_send(&TWIE, PCA9557_BASE_ADDRESS+addr, PCA9557_DIRECTION_REGISTER, state);
+}
+
+void pca9557_set_pin_level(uint8_t addr, uint8_t port, uint8_t level)
+{
+	uint8_t state;
+	state = i2c_read(&TWIE,PCA9557_BASE_ADDRESS+addr,PCA9557_OUTPUT_REGISTER);
+	if (level == PCA9557_PIN_LEVEL_LOW)
+		state = state | port;
+	else if (level == PCA9557_PIN_LEVEL_HIGH)
+		state = state & port;
+	i2c_send(&TWIE, PCA9557_BASE_ADDRESS+addr, PCA9557_OUTPUT_REGISTER, state);
+};
+
+uint8_t pca9557_get_pin_level(int fd, uint8_t addr, uint8_t port)
+{
+	uint8_t state;
+	ioctl(fd,I2C_SLAVE,PCA9557_BASE_ADDRESS+addr)
+	state = i2c_smbus_read_byte_data(fd,PCA9557_INPUT_REGISTER);
+	return state | ~port;
+}
+
 int main(int argc, char *argv[])
 {
 	int fd;
